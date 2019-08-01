95d1844ad933ff478f572d23281f73dfc71f8e47
Ahmed Ibrahim <49079836+Ahmed512w@users.noreply.github.com>
2019-08-01 23:27:12 +0200

    update variable_(querty)

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -4,7 +4,7 @@ import psycopg2
 
 DBNAME = "news"
 
-articles = '''
+top_3_articles = '''
     SELECT article, count(*) AS views
     FROM   log_article_auther
     GROUP BY article
@@ -13,7 +13,7 @@ articles = '''
     ;
     '''
 
-authors = '''
+top_authors = '''
     SELECT author, count(*) AS views
     FROM   log_article_auther
     GROUP BY author
@@ -21,7 +21,7 @@ authors = '''
     ;
     '''
 
-errors = '''
+errors_percent = '''
     SELECT day, percent
     FROM error_percent
     WHERE percent > 1
@@ -38,14 +38,14 @@ def get(query):
 
 
 def popular_3_articles():
-    popular_3_articles = get(articles)
+    popular_3_articles = get(top_3_articles)
     print("\nMost popular three articles of all time\n")
     for title, num in popular_3_articles:
         print('''\t"{}" -- {} views'''.format(title, num))
 
 
 def popular_authors():
-    popular_authors = get(authors)
+    popular_authors = get(top_authors)
     print("\nMost popular article authors of all time:\n")
     for title, num in popular_authors:
         print('''\t"{}" -- {} views'''.format(title, num))
@@ -53,7 +53,7 @@ def popular_authors():
 
 
 def high_error_day():
-    high_error_day = get(errors)
+    high_error_day = get(errors_percent)
     print("\nDays with more than 1% of requests lead to errors:\n")
     for day, per in high_error_day:
         print('''\t{0:%B %d, %Y} -- {1:.2f} % errors'''.format(day, per))
